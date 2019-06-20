0bf84ab2b1ba43b74a254b9cb775abd5ff4e0b9a
Bohdan Kucheriavyi <kucheriavyi.w32@gmail.com>
2019-06-20 16:13:03 +0300

    Updates

---
+++ precache-manifest.da46a34b1702eddc2fcefc77dfdb2e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c4e12622e568cb3affcaec2725d4c0",
+    "url": "/headway-landing-page/index.html"
+  },
+  {
+    "revision": "eb36e2bdee0bee932abe",
+    "url": "/headway-landing-page/static/css/main.856edd03.chunk.css"
+  },
+  {
+    "revision": "0a76e31368bdc65a8b80",
+    "url": "/headway-landing-page/static/js/2.59eb3c13.chunk.js"
+  },
+  {
+    "revision": "eb36e2bdee0bee932abe",
+    "url": "/headway-landing-page/static/js/main.d666ee75.chunk.js"
+  },
+  {
+    "revision": "eccfe6e79204bf089141",
+    "url": "/headway-landing-page/static/js/runtime~main.1eb4709b.js"
+  },
+  {
+    "revision": "82393efec51e3fab12ccc31e7a7b57e5",
+    "url": "/headway-landing-page/static/media/modal-wallpaper@1X.82393efe.jpeg"
+  }
+]);
