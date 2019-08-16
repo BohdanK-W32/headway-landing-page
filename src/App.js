947a0fb0c09973f7e3d6192fe9792f6c3887a779
Bohdan Kucheriavyi <kucheriavyi.w32@gmail.com>
2019-08-16 21:27:43 +0300

    fb script Lead

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Page from './components/Page/Page';
+import FacebookScripts from './services/facebookScripts';
 
 const App = () => (
   <div>
+    <Route path="/thanks" component={FacebookScripts} />
     <Route path="/" component={Page} />
   </div>
 );
---
+++ src/services/facebookScripts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// eslint-disable-next-line
+export const LeadScript = () => <script>fbq('track', 'Lead')</script>;
+
+const FacebookScripts = () => (
+  <>
+    <LeadScript />
+  </>
+);
+
+export default FacebookScripts;
